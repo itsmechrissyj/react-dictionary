4e972fd6520ba29ff752cd53e1ee0cb967266797
itsmechrissyj <123317393+itsmechrissyj@users.noreply.github.com>
2023-04-11 06:43:01 +0100

    adding axios and dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,14 +7,14 @@ export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
 
     function handleResponse(response) {
-console.log(response);
+console.log(response.data);
 
     }
     function search(event) {
         event.preventDefault();
     alert (`Searching for ${keyword} definition`);
 
-    let apiUrl - "https://api.dictionaryapi.dev/api/v2/entries/en/<word>";
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
 }
 function handleKeywordChange (event){
